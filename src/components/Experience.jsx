7c6b2870af74be2d2e314bf7379e00249637468a
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-05-02 09:15:51 +0300

    fiverr update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,21 +15,75 @@ export default function Experience() {
         <div>
           <h1 id="experience">Work Experiences </h1>
           <div className="Card_list projects-list">
-            <Card>
-              <h2>Fiverr</h2>
-              <span>2021-PRESENT</span>
-              <p>
-                Freelancer: Website & App Design/Development <br />
-                <a
-                  href="https://www.fiverr.com/share/V8Qyze"
-                  target="_blank"
-                  rel="noreferrer"
+            <div style={{position:"relative"}}>
+              {" "}
+              <Card>
+                <h2>Fiverr</h2>
+                <span>2021-PRESENT</span>
+                <p>
+                  Freelancer: Website & App Design/Development <br />
+                  <a
+                    href="https://www.fiverr.com/share/V8Qyze"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View site ≫
+                  </a>{" "}
+                </p>
+                <img src="/Images/fiverr.gif" />
+
+                <div
+                  itemscope
+                  itemtype="http://schema.org/Person"
+                  class="fiverr-seller-widget"
+                  style={{
+                    display: "inline-block",
+                    position: "absolute",
+                    height:70,
+                    top: 0,
+                    right:100,
+                  }}
                 >
-                  View site ≫
-                </a>{" "}
-              </p>
-              <img src="/Images/fiverr.gif" />
-            </Card>
+                  <a
+                    itemprop="url"
+                    href="https://www.fiverr.com/tokumakiyu"
+                    rel="nofollow"
+                    target="_blank"
+                    style={{ display: "inline-block" }}
+                  >
+                    <div
+                      class="fiverr-seller-content"
+                      id="fiverr-seller-widget-content-ae6fcfc1-1adc-4131-97b8-d0aad9ece3d4"
+                      itemprop="contentURL"
+                      style={{ display: "none" }}
+                    ></div>
+                    <div
+                      id="fiverr-widget-seller-data"
+                      style={{ display: "none" }}
+                    >
+                      <div itemprop="name">tokumakiyu</div>
+                      <div itemscope itemtype="http://schema.org/Organization">
+                        <span itemprop="name">Fiverr</span>
+                      </div>
+                      <div itemprop="jobtitle">Seller</div>
+                      <div itemprop="description">
+                        🚀 Web Developer | Software Engineer Welcome! I'm Tokuma
+                        Abdisa, a web developer with a strong Software
+                        Engineering background. with 5+ experience, I've
+                        specialized in: Responsive Web Design User Interaction
+                        and Secure Logins Cross-Platform Compatibility
+                        Client-Centric Approach: I tailor solutions to your
+                        goals, be it an e-commerce platform, dynamic web app, or
+                        blog. Client Satisfaction: On-time delivery and budget
+                        adherence are my priorities. Open communication and your
+                        feedback guide my process. Let's bring your digital
+                        vision to life! 💻✨
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </Card>
+            </div>
             <Card>
               <h2>Upwork</h2>
               <span>2021-PRESENT</span>
